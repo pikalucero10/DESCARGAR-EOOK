952f4584d9a692924757bb75583ae9db340e71a7
lucerodarionetflix <lucerodarionetflix@gmail.com>
2024-08-04 15:00:26 -0300

    hola

---
+++ main.js
@@ -0,0 +1,19 @@
+
+
+document.getElementById('downloadBtn').addEventListener('click', function(e) {
+  e.preventDefault();
+  
+  // Cambia el texto del botón
+  this.textContent = 'Descargando...';
+  
+  // Simula un proceso de descarga
+  setTimeout(() => {
+    this.textContent = '¡Descarga Completada!';
+    this.style.backgroundColor = '#45a049';
+    
+    // Redirige después de un breve retraso
+    setTimeout(() => {
+      window.location.href = 'https://cuty.io/arz0k';
+    }, 1000);
+  }, 2000);
+});
